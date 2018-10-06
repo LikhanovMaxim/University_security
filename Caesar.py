b845733596f00c28521276a1d1774a91e27c2761
Likhanov Maxim <Maksim_Likhanov@epam.com>
2018-10-06 20:22:21 +0300

    first task completed: decrypt a text by cipher Caesar

--- Caesar.py
+++ Caesar.py
@@ -1,3 +1,7 @@
+from read_write_file import read_data_1byte as read
+from read_write_file import write_data_1byte as write
+import detectEnglish
+
 COUNT = 256
 
 
@@ -25,3 +29,68 @@ def decrypt_data(data_c, key):
         m = decrypt(symbol, key)
         data.append(m)
     return data
+
+
+def first_example():
+    key = 37
+    m = 24
+    c = encrypt(m, key)
+    print('c=', c)
+    m1 = decrypt(c, key)
+    print('m1=', m1)
+    data = [34, 67, 123, 79, 201]
+    encrypt_bytes = encrypt_data(data, key)
+    print('encrypt_data =', encrypt_bytes)
+    decrypt_bytes = decrypt_data(encrypt_bytes, key)
+    print('decrypt_data =', decrypt_bytes)
+
+
+def decrypt_file_example(encrypt_txt, decrypt_txt, key):
+    encrypt_bytes = read(encrypt_txt)
+    print('encrypt_data=', encrypt_bytes)
+
+    decrypt_bytes = decrypt_data(encrypt_bytes, key)
+    print('decrypt_data=', decrypt_bytes)
+
+    print('decrypt_text=', transform_code_to_text(decrypt_bytes))
+
+    write(decrypt_txt, decrypt_bytes)
+
+
+def encrypt_file_example(file, encrypt_txt, key):
+    source_text = read(file)
+    print('source_byte=', source_text[0:15])
+
+    # text = transform_to_text(source_text)
+    text = transform_code_to_text(source_text)
+    print('source_text=', text)
+
+    encrypt_text = encrypt_data(source_text, key)
+    print('encrypt_byte=', encrypt_text[0:15])
+
+    print('encrypt_text=', transform_code_to_text(encrypt_text))
+
+    write(encrypt_txt, encrypt_text)
+
+
+def transform_code_to_text(source_text):
+    return ''.join([chr(s) for s in source_text])
+
+
+# Task 1: decrypt a file
+def decrypt_file(encrypt_file, save_file):
+    encrypt_bytes = read(encrypt_file)
+    key = -1
+    for i in range(0, COUNT):
+        decrypt_bytes = decrypt_data(encrypt_bytes[0:20], i)
+        text = transform_code_to_text(decrypt_bytes)
+        print('decrypt_data=', text)
+        print('key=', i)
+        if detectEnglish.isEnglish(text):
+            print('Find! Key=', i)
+            key = i
+            break
+    if key != -1:
+        write(save_file, decrypt_data(encrypt_bytes, key))
+    else:
+        print('We could not find a key :(')
--- main_caesar.py
+++ main_caesar.py
@@ -1,48 +1,28 @@
 import Caesar
-# import read_write_file
+import detectEnglish
 from read_write_file import read_data_1byte as read
 from read_write_file import write_data_1byte as write
 
+KEY = 67
+FILE = 'f1.txt'
+ENCRYPT_TXT = 'f1_encrypt.txt'
+DECRYPT_TXT = 'f1_decrypt.txt'
+ENCRYPT_FOR_TASK_1 = 't3_caesar_c_all.txt'
+RESULT_TXT = 'task_1_result.txt'
+
 
 def main():
-    source_text = read('f1.txt')
-    print('source_byte=', source_text[0:15])
-
-    # text = transform_to_text(source_text)
-    text = transform_code_to_text(source_text)
-    print('source_text=', text)
-
-    encrypt_text = Caesar.encrypt_data(source_text, key=67)
-    print('encrypt_byte=', encrypt_text[0:15])
-
-    print('encrypt_text=', transform_code_to_text(encrypt_text))
-
-    write('f1_encrypt.txt', encrypt_text)
+    # Task 1
+    Caesar.decrypt_file(ENCRYPT_FOR_TASK_1, 'task_1_result.txt')
+    # Caesar.decrypt_file_example(ENCRYPT_FOR_TASK_1, RESULT_TXT, 134)
 
 
-def transform_code_to_text(source_text):
-    return ''.join([chr(s) for s in source_text[0:15]])
+def decrypt_file_example():
+    Caesar.decrypt_file_example(ENCRYPT_TXT, DECRYPT_TXT, KEY)
 
 
-def transform_to_text(data):
-    text = ''
-    for i in data[0:15]:
-        text += chr(i)
-    return text
-
-
-def first_task():
-    key = 37
-    m = 24
-    c = Caesar.encrypt(m, key)
-    print('c=', c)
-    m1 = Caesar.decrypt(c, key)
-    print('m1=', m1)
-    data = [34, 67, 123, 79, 201]
-    encrypt_data = Caesar.encrypt_data(data, key)
-    print('encrypt_data =', encrypt_data)
-    decrypt_data = Caesar.decrypt_data(encrypt_data, key)
-    print('decrypt_data =', decrypt_data)
+def encrypt_file_example():
+    Caesar.encrypt_file_example(FILE, ENCRYPT_TXT, KEY)
 
 
 if __name__ == '__main__':
